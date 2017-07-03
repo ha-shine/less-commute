c34214441b331a7df64f245aca01867435cf3838
Htet Aung Shine <has.htetaungshine1994@gmail.com>
2017-07-04 00:38:38 +0800

    Refactored out mergeProps from MainForm

--- mobile/src/components/MainForm.tsx
+++ mobile/src/components/MainForm.tsx
@@ -7,25 +7,51 @@ import './MainForm.css';
 import AutocompletePrediction = google.maps.places.AutocompletePrediction;
 import IdentifiableDirectionsRoute from '../entities/IdentifiableDirectionsRoute';
 import * as constants from '../constants/index';
+import {CurrentModal} from '../constants/index';
+import DirectionsResult = google.maps.DirectionsResult;
+import {getGoogleDirection} from '../services/index';
+import {
+    FetchGoogleRouteAction, RemoveHomeAddressAction, SelectHomeAddressAction,
+    SelectWorkAddressAction, SetDaysAction, ShowModalAction
+} from '../actions/index';
 
 export interface Props {
     selectedWorkAddress: AutocompletePrediction | null;
     selectedHomeAddress: AutocompletePrediction | null;
-    onSelectHomeAddress: (address: AutocompletePrediction) => void;
-    onSelectWorkAddress: (address: AutocompletePrediction) => void;
-    onRemoveHomeAddress: () => void;
-    onRemoveWorkAddress: () => void;
-    onReceiveRouteFromSource: (route: IdentifiableDirectionsRoute[]) => void;
-    onReceiveRouteFromDestination: (route: IdentifiableDirectionsRoute[]) => void;
-    onShowModal: (s: constants.CurrentModal) => void;
-    onClickCalculateBtn: () => void;
-    setDays: (days: number) => void;
+    onSelectHomeAddress: (address: AutocompletePrediction) => SelectHomeAddressAction;
+    onSelectWorkAddress: (address: AutocompletePrediction) => SelectWorkAddressAction;
+    onRemoveHomeAddress: () => RemoveHomeAddressAction;
+    onRemoveWorkAddress: () => RemoveHomeAddressAction;
+    onReceiveRouteFromSource: (route: IdentifiableDirectionsRoute[]) => FetchGoogleRouteAction;
+    onReceiveRouteFromDestination: (route: IdentifiableDirectionsRoute[]) => FetchGoogleRouteAction;
+    onShowModal: (s: constants.CurrentModal) => ShowModalAction;
+    setDays: (days: number) => SetDaysAction;
 }
 export default class MainForm extends React.Component<Props, {}> {
     private daysInput: HTMLInputElement;
     onClickCalculate= () => {
         this.props.setDays(Number(this.daysInput.value));
-        this.props.onClickCalculateBtn();
+        this.props.onShowModal(CurrentModal.BaseRoute);
+        let departureTimeFromSource = new Date();
+        let departureTimeFromDestination = new Date();
+        departureTimeFromSource.setHours(8);
+        departureTimeFromDestination.setHours(18);
+        const callbackFromSource = (result: DirectionsResult) => {
+            const directionsRoutes = result.routes.map((route) => {
+                return new IdentifiableDirectionsRoute(route);
+            });
+            this.props.onReceiveRouteFromSource(directionsRoutes);
+        };
+        const callbackFromDestination = (result: DirectionsResult) => {
+            const directionsRoutes = result.routes.map((route) => {
+                return new IdentifiableDirectionsRoute(route);
+            });
+            this.props.onReceiveRouteFromDestination(directionsRoutes);
+        };
+        const source = this.props.selectedHomeAddress as AutocompletePrediction;
+        const destination = this.props.selectedWorkAddress as AutocompletePrediction;
+        getGoogleDirection(source, destination, departureTimeFromSource, callbackFromSource);
+        getGoogleDirection(destination, source, departureTimeFromDestination, callbackFromDestination);
     }
     render() {
         const shouldDisableCalculateButton = this.props.selectedHomeAddress == null ||
--- mobile/src/containers/MainForm.ts
+++ mobile/src/containers/MainForm.ts
@@ -8,34 +8,16 @@ import {
 } from '../actions/index';
 import AutocompletePrediction = google.maps.places.AutocompletePrediction;
 import MainForm from '../components/MainForm';
-import DirectionsResult = google.maps.DirectionsResult;
-import {getGoogleDirection} from '../services/index';
 import * as constants from '../constants/index';
-import {CurrentModal} from '../constants/index';
 import IdentifiableDirectionsRoute from '../entities/IdentifiableDirectionsRoute';
 /**
  * Created by Shine on 6/29/2017.
  */
-interface StateProps {
-    selectedHomeAddress: AutocompletePrediction| null;
-    selectedWorkAddress: AutocompletePrediction| null;
-}
 function mapStateToProps(s: StoreState) {
     return {
         selectedHomeAddress: s.selectedHomeAddress,
         selectedWorkAddress: s.selectedWorkAddress
     };
-}
-
-interface DispatchProps {
-    onSelectHomeAddress: (a: AutocompletePrediction) => void;
-    onRemoveHomeAddress: () => void;
-    onSelectWorkAddress: (a: AutocompletePrediction) => void;
-    onRemoveWorkAddress: () => void;
-    onReceiveRouteFromSource: (route: IdentifiableDirectionsRoute[]) => void;
-    onReceiveRouteFromDestination: (route: IdentifiableDirectionsRoute[]) => void;
-    onShowModal: (s: constants.CurrentModal) => void;
-    setDays: (days: number) => void;
 }
 function mapDispatchToProps(dispatch: Dispatch<object>) {
     return {
@@ -51,36 +33,4 @@ function mapDispatchToProps(dispatch: Dispatch<object>) {
     };
 }
 
-function onClickCalculateButton(stateProps: StateProps, dispatchProps: DispatchProps,) {
-    dispatchProps.onShowModal(CurrentModal.BaseRoute);
-    let departureTimeFromSource = new Date();
-    let departureTimeFromDestination = new Date();
-    departureTimeFromSource.setHours(8);
-    departureTimeFromDestination.setHours(18);
-    const callbackFromSource = (result: DirectionsResult) => {
-        const directionsRoutes = result.routes.map((route) => {
-            return new IdentifiableDirectionsRoute(route);
-        });
-        dispatchProps.onReceiveRouteFromSource(directionsRoutes);
-    };
-    const callbackFromDestination = (result: DirectionsResult) => {
-        const directionsRoutes = result.routes.map((route) => {
-            return new IdentifiableDirectionsRoute(route);
-        });
-        dispatchProps.onReceiveRouteFromDestination(directionsRoutes);
-    };
-    const source = stateProps.selectedHomeAddress as AutocompletePrediction;
-    const destination = stateProps.selectedWorkAddress as AutocompletePrediction;
-    getGoogleDirection(source, destination, departureTimeFromSource, callbackFromSource);
-    getGoogleDirection(destination, source, departureTimeFromDestination, callbackFromDestination);
-}
-
-function mergeProps(stateProps: StateProps, dispatchProps: DispatchProps) {
-    return {
-        ...stateProps,
-        ...dispatchProps,
-        onClickCalculateBtn: () => onClickCalculateButton(stateProps, dispatchProps)
-    };
-}
-
-export default connect(mapStateToProps, mapDispatchToProps, mergeProps)(MainForm);
+export default connect(mapStateToProps, mapDispatchToProps)(MainForm);
