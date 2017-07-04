11f54e928df7ece0b8cfcbb00d37f614c3cacee9
Htet Aung Shine <has.htetaungshine1994@gmail.com>
2017-07-05 00:23:55 +0800

    Appearance changes for RouteCompareMenu

--- mobile/src/components/RouteCompareMenu.tsx
+++ mobile/src/components/RouteCompareMenu.tsx
@@ -9,7 +9,7 @@ interface Props {
 export function RouteCompareMenu(p: Props) {
     if (p.baseRoutes !== null) {
         return (
-            <div>
+            <div className="route-compare-menu">
                 <RouteCompareRow routePair={p.baseRoutes} days={p.days} />
             </div>
         );
@@ -23,8 +23,14 @@ function RouteCompareRow(p: {routePair: DirectionsRoutePair, days: number}) {
     return (
         <div className="row comparer-row">
             <div className="col-xs-8">{p.routePair.address.description}</div>
-            <div className="col-xs-2">{totalCost}</div>
-            <div className="col-xs-2">{totalDuration}</div>
+            <div className="col-xs-2 duration">
+                <div className="value">{totalDuration}</div>
+                <div className="identifier">min</div>
+            </div>
+            <div className="col-xs-2 cost">
+                <div className="value">{totalCost}</div>
+                <div className="identifier">SGD</div>
+            </div>
         </div>
     );
 }
